96171d97881394f6954df3dd671af99a55cf6684
avrahambraun <abbraun@mail.yu.edu>
2016-02-07 23:26:21 -0500

    Update dblout.c

--- dblout.c
+++ dblout.c
@@ -1,3 +1,6 @@
+/* a program the book uses to help demonstrate redirection. The program outputs two characters for whatever character you type on 
+the line below. Must use end-of-file Signal, like ctrl d, or ctrl c to terminate.*/
+
 #include <stdio.h>
 
 int main(void)
